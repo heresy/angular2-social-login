e2d20dc70cc8bbbd66fdac1d8d9a9e9a7cd4b83e
Daniel Ip <kg4368@yahoo.com.hk>
2017-09-18 21:54:32 +1000

    Implement Promises for loading sdks

--- src/angular2-social-login.module.ts
+++ src/angular2-social-login.module.ts
@@ -9,55 +9,68 @@ declare let FB: any;
     providers: [AuthService]
 })
 export class Angular2SocialLoginModule{
-    static loadProvidersScripts(config: IProviders): void{
+    static loadProvidersScripts(config: IProviders): Promise<any>{
         const loadProvidersScripts: Object = {
             google: (info: IProvider) => {
-                let d = document, gJs, ref: any = d.getElementsByTagName('script')[0];
-                gJs = d.createElement('script');
-                gJs.async = true;
-                gJs.src = "//apis.google.com/js/platform.js";
-
-                gJs.onload = function() {
-                    gapi.load('auth2', function() {
-                        gapi.auth2.init({
-                        client_id: info["clientId"],
-                        scope: 'email'
+                return new Promise((resolve, reject)=>{
+                    let d = document, gJs, ref: any = d.getElementsByTagName('script')[0];
+                    gJs = d.createElement('script');
+                    gJs.async = true;
+                    gJs.src = "//apis.google.com/js/platform.js";
+    
+                    gJs.onload = function() {
+                        gapi.load('auth2', function() {
+                            gapi.auth2.init({
+                            client_id: info["clientId"],
+                            scope: 'email'
+                            })
+                            resolve(true);
                         })
-                    })
-                }
-                ref.parentNode.insertBefore(gJs, ref);
+                    }
+                    ref.parentNode.insertBefore(gJs, ref);
+                });
             },
             linkedin: (info: IProvider) => {
-                let lIN, d = document, ref: any = d.getElementsByTagName('script')[0];
-                lIN = d.createElement('script');
-                lIN.async = false;
-                lIN.src = "//platform.linkedin.com/in.js";
-                lIN.text = ("api_key: " + info["clientId"]).replace("\"", "");
-                ref.parentNode.insertBefore(lIN, ref);
+                return new Promise((resolve, reject)=>{
+                    let lIN, d = document, ref: any = d.getElementsByTagName('script')[0];
+                    lIN = d.createElement('script');
+                    lIN.async = false;
+                    lIN.src = "//platform.linkedin.com/in.js";
+                    lIN.text = ("api_key: " + info["clientId"]).replace("\"", "");
+                    ref.parentNode.insertBefore(lIN, ref);
+                    resolve(true);
+                });
             },
             facebook: (info: IProvider) => {
-                let d = document, fbJs, id = 'facebook-jssdk', ref: any = d.getElementsByTagName('script')[0];
-                fbJs = d.createElement('script');
-                fbJs.id = id;
-                fbJs.async = true;
-                fbJs.src = "//connect.facebook.net/en_US/sdk.js";
-
-                fbJs.onload = function() {
-                    FB.init({
-                        appId: info["clientId"],
-                        status: true,
-                        cookie: true,
-                        xfbml: true,
-                        version: info["apiVersion"]
-                    });
-                };
-
-                ref.parentNode.insertBefore(fbJs, ref);
+                return new Promise((resolve, reject)=>{
+                    let d = document, fbJs, id = 'facebook-jssdk', ref: any = d.getElementsByTagName('script')[0];
+                    fbJs = d.createElement('script');
+                    fbJs.id = id;
+                    fbJs.async = true;
+                    fbJs.src = "//connect.facebook.net/en_US/sdk.js";
+    
+                    fbJs.onload = function() {
+                        FB.init({
+                            appId: info["clientId"],
+                            status: true,
+                            cookie: true,
+                            xfbml: true,
+                            version: info["apiVersion"]
+                        });
+                        resolve(true);
+                    };
+    
+                    ref.parentNode.insertBefore(fbJs, ref);
+                });
             }
         }
 
+        let promises:Array<Promise<any>> = [];
+
         Object.keys(config).forEach((provider: string) => {
-            (<any>loadProvidersScripts)[provider](config[provider]);
+            promises.push((<any>loadProvidersScripts)[provider](config[provider]));
         });
+
+        return Promise.all(promises);
     }
 }
